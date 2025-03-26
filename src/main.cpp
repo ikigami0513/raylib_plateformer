1607b8cb88288dc7affbd305924e8d9bfa430ee4
ethan-liberfit <ethan.rucar@liber.fit>
2025-03-26 17:40:42 +0100

    delete Player struct

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,6 @@
 #define PLAYER_JUMP_SPD 350.0f
 #define PLAYER_HOR_SPD 200.0f
 
-struct Player {
-	Vector2 position;
-	float speed;
-	bool canJump;
-};
-
 class Player {
 	public:
 		Vector2 position;
